745bf1d3a0c8dc6e2fa19d9ba438960e63246b3e
Gopal Pawar <pawar.gopal9977@gmail.com>
2026-06-26 17:26:58 +0530

    reflection code

---
+++ ReflectionCode.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.*;
+class Demo
+{
+	private Demo()
+	{
+		System.out.println("private Constructor");
+	}
+	private void show()
+	{
+		System.out.println("private method");
+	}
+}
+
+class Reflection
+{
+public static void main(String ar[])throws Exception
+{
+Class c = Demo.class;
+
+Constructor con=c.getDeclaredConstructor();
+
+con.setAccessible(true);
+
+Method m=c.getDeclaredMethod("show");
+
+m.setAccessible(true);
+
+m.invoke(con.newInstance());
+
+}
+}
